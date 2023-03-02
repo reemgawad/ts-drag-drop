6cfe580507140923efd29986f08b113c8b9e5bc6
Reem Gawad <reemabdelg@gmail.com>
2023-03-02 13:39:35 -0500

    re-organized files with namespaces

---
+++ src/components/project-input.ts
@@ -0,0 +1,84 @@
+/// <reference path="base-component.ts"/>
+/// <reference path="../util/validation.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../state/project-state.ts" />
+
+// ProjectInput Class
+namespace App {
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+      super("project-input", "app", true, "user-input");
+      this.titleInputElement = this.element.querySelector(
+        "#title"
+      ) as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector(
+        "#description"
+      ) as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector(
+        "#people"
+      ) as HTMLInputElement;
+
+      this.configure();
+    }
+
+    configure() {
+      this.element.addEventListener("submit", this.submitHandler);
+    }
+
+    fillContent(): void {}
+
+    private getUserInput(): [string, string, number] | void {
+      const userTitle = this.titleInputElement.value;
+      const userDescription = this.descriptionInputElement.value;
+      const userPeople = this.peopleInputElement.value;
+
+      const validatableTitle: Validatable = {
+        value: userTitle,
+        required: true,
+      };
+      const validatableDesc: Validatable = {
+        value: userDescription,
+        required: true,
+        minLength: 5,
+      };
+      const validatablePeople: Validatable = {
+        value: userPeople,
+        required: true,
+        min: 1,
+        max: 5,
+      };
+
+      if (
+        !validate(validatableTitle) ||
+        !validate(validatableDesc) ||
+        !validate(validatablePeople)
+      ) {
+        alert("This is not a valid input.");
+        return;
+      } else {
+        return [userTitle, userDescription, +userPeople];
+      }
+    }
+
+    private clearInputs() {
+      this.titleInputElement.value = "";
+      this.descriptionInputElement.value = "";
+      this.peopleInputElement.value = "";
+    }
+
+    @Autobind
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      const userInput = this.getUserInput();
+      if (Array.isArray(userInput)) {
+        const [title, desc, people] = userInput;
+        projectState.addProject(title, desc, people);
+        this.clearInputs();
+      }
+    }
+  }
+}
---
+++ src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+  // Autobind Decorator
+  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const newMethod: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return newMethod;
+  }
+}
