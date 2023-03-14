913e4bb78dda8d023a1c4730ff9dd56f3a65c088
Reem Gawad <reemabdelg@gmail.com>
2023-03-14 12:29:44 -0400

    Webpack setup for dev & prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,14 @@
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.ts",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
   },
-  devtool: "inline-source-map",
+  devtool: "hidden-source-map",
   module: {
     rules: [
       {
@@ -28,4 +21,5 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()]
 };
